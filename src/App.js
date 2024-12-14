8bcf46ac414326998d055248bf475df3ee3e48b5
TaarDenn <taar.denn@gmail.com>
2024-12-14 05:44:32 +0330

    setup g-pages

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/horizontal" element={<Alt16 />} />
-        <Route exact path="/vertical" element={<Alt12 />} />z
+        <Route exact path="/vertical" element={<Alt12 />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
+const Home = () => {
+  return (
+    <div>
+      <a className="block" href="/horizontal">Horizontal</a>
+      <a className="block" href="/vertical">Vertical</a>
+    </div>
+  );
+};
 export default App;
